2a2503a901bcaf2ab724d7875382ede2b35d8458
iconocam <150183122+iconocam@users.noreply.github.com>
2023-12-27 15:03:18 -0500

    robin's companions can also roll now

--- script.js
+++ script.js
@@ -42,3 +42,32 @@ What if we had many adventurers? As you can imagine, creating several of these o
 Next, we will level up our approach using Classes. */
 
 // adventurer.roll();  works successfully
+
+// Part 2: Class Fantasy
+
+/** Let’s take a moment to analyze the data above. What are the common features of each object?
+When creating classes, begin by searching for the simplest form your data takes. Remember, you can add specificity later by extending the classes.
+
+Start with a Character class, which will define generic character entities. Robin and their companions all have a name, so the Character class should definitely include name as one of its properties. At this stage, we will also make the decision that every character should have health (which we will standardize to a maximum of 100, and an inventory (even if the inventory is empty). */
+
+// Here is what the basic Character class looks like so far, including a constructor function that allows us to create new characters with whatever name we would like: //
+class Character {
+constructor (name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+}
+roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.subCompanion = new Character("Frank");
+robin.companion.subCompanion.type = "Flea";
+robin.companion.subCompanion.inventory = ["small hat", "sunglasses"];
+
+// robin.companion.roll(5) 
